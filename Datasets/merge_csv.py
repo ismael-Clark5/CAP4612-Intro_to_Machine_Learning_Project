69aa2307be8188e9254040502372a8a6e5d9a3ea
Hector R <github.hector1@gmail.com>
2021-10-21 12:11:23 -0400

    Added function to get X,Y Splits

--- Datasets/merge_csv.py
+++ Datasets/merge_csv.py
@@ -4,12 +4,13 @@ import pandas as pd;
 import os
 import random as rd
 from sklearn.model_selection import train_test_split
+import typing.Tuple
 
 training_df = pd.DataFrame();
 testing_df = pd.DataFrame()
 random = rd.Random()
 
-def loadToMem(df: pd.DataFrame, cancer: str):
+def split(df: pd.DataFrame, cancer: str): 
     df.insert(1, 'cancer', cancer)
     training, testing = train_test_split(df, train_size=0.8, test_size=0.2)
 
@@ -27,7 +28,11 @@ for file in os.listdir(os.getcwd()):
         file_df = pd.read_csv(file)
         print(f'Cancer: {cancer}, # Rows {len(file_df.index)}')
         count += len(file_df.index)
-        loadToMem(file_df, cancer)
+        split(file_df, cancer)
+
+def getSplits():
+    X_train, X_test, Y_train, Y_test = train_test_split(training_df, testing_df, test_size=0.2)
+    return (X_train, X_test, Y_train, Y_test)
 
 print(f'Total Rows Accross all Files: {count}')
 print(f'Training Data # Rows: {len(training_df.index)}')
