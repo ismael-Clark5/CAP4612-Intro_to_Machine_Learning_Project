d5484ed43b831fd60d690967ad62b3665e22ec6a
Hector R <github.hector1@gmail.com>
2021-10-21 17:49:31 -0400

    Modularized the events in lasso, codified the mapping of cancer to class number, and reversed to be able to go both ways

--- ML Models/data.py
+++ ML Models/data.py
@@ -4,6 +4,21 @@ from sklearn.model_selection import train_test_split
 training_df = pd.read_csv("../Datasets/training_merged.csv")
 testing_df = pd.read_csv("../Datasets/testing_merged.csv")
 
+cancer_to_int = {
+    'BRCA': 1,
+    'CHOL' : 2,
+    'COAD' : 3,
+    'KICH' : 4,
+    'KIRC' : 5,
+    'KIRP' : 6,
+    'LIHC' : 7,
+    'LUAD' : 8,
+    'LUSC' : 9,
+    'PRAD' : 10,
+    'READ' : 11,
+    'THCA' : 12
+}
+int_to_cancer = {v: k for k, v in cancer_to_int.items()}
 def getSplits():
     unified_df = training_df.append(testing_df)
     X_train, X_test, Y_train, Y_test = train_test_split(unified_df, test_size=0.2)
--- ML Models/lasso.py
+++ ML Models/lasso.py
@@ -1,32 +1,60 @@
-from sklearn.model_selection import cross_val_score
+from typing import Any
 from sklearn.model_selection import RepeatedKFold
-from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import Lasso
 from sklearn.linear_model import LassoCV
+from sklearn.linear_model import Lasso
 import numpy as np
 import data as d
+import os
 
-lasso_alphas = np.linspace(0,0.2, 11)
+## Function used to find the optimal alpha value for LASSO for the given training set.
+def findOptimalAlpha() -> Any:
+    grid=dict()
+    grid['alpha'] = np.arange(0,1,0.01)
+    cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+    labels = d.training_df['cancer']
+    clean_df=d.training_df.drop(labels=['Ensembl_ID', 'cancer', 'Unnamed: 0'], axis=1)
+    lasso = LassoCV(alphas=np.arange(0,1,0.01), cv=cv, n_jobs=8)
+    results2 = lasso.fit(clean_df, labels)
+    print('Alpha: %.3f' % results2.alpha_)
+    ##Store our computed alpha value, we dont wanna do extra work if not needed
+    f = open('alpha.csv', mode='w')
+    f.write(f'{results2.alpha_}')
+    return results2
 
+def fitModel(model) -> Any:
+    labels = d.training_df['cancer']
+    clean_df=d.training_df.drop(labels=['Ensembl_ID', 'cancer', 'Unnamed: 0'], axis=1)
+    return model.fit(clean_df, labels)
 
-model = Lasso()
-grid=dict()
-grid['alpha'] = np.arange(0,1,0.01)
-cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
-labels = d.training_df['cancer']
-ensamble_ids=d.training_df['Ensembl_ID']
-unamed = d.training_df['Unnamed: 0']
-clean_df=d.training_df.drop(labels=['Ensembl_ID', 'cancer', 'Unnamed: 0'], axis=1)
-#print(clean_df)
-#print(labels)
-lasso = LassoCV(alphas=np.arange(0,1,0.1), cv=cv, n_jobs=8)
-#search = GridSearchCV(model, grid, scoring="neg_mean_absolute_error", cv=cv, n_jobs=4)
-#scores = cross_val_score(estimator=model, X=clean_df, y=labels, scoring="neg_mean_absolute_error", cv=cv, n_jobs=4)
-#print(scores)
-#scores = np.absolute(scores)
-#print(scores)
-#print( 'Mean MEA: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
-#results = search.fit(clean_df, labels)
-results2 = lasso.fit(clean_df, labels)
-print('Alpha: %.3f' % results2.alpha_)
-#print('Config: %s' % results2.best_params_)
+def predict(model, row, labels):
+    prediction = model.predict(row)
+    total_count = len(prediction)
+    total_correct = 0
+    total_wrong = 0
+    for x in range(len(prediction)):
+        prediction[x] = np.abs(np.round(prediction[x]))
+        if prediction[x] == labels[x]:
+            total_correct += 1
+        else:
+            total_wrong  += 1
+    print(f'Predicted Class: {prediction}')
+    print(f'Total Predictions: {total_count}, Total Correct: {total_correct} Correct %: { (total_correct / total_count) * 100}')
+
+def init():
+    alpha=0.0
+    model = Lasso()
+    if(os.path.exists('alpha.csv')):
+        print('Found alpha file, loading alpha value from file')
+        alpha = float(open('alpha.csv').read())
+        print(f'Fitting Model with red alpha of {alpha}')
+        model = fitModel(model=Lasso(alpha=alpha))
+    else:
+        print('Alpha File not Found, Computing Alpha value from training data. (This will take a long time)')
+        model = findOptimalAlpha()
+        alpha = model.alpha_
+    labels = d.testing_df['cancer']
+    print(f'Labels len: {len(labels)}')
+    clean_df = d.testing_df.drop(labels=['Ensembl_ID', 'cancer', 'Unnamed: 0'], axis=1)
+    predict(model, clean_df, labels)
+if __name__ == '__main__':
+    init()
