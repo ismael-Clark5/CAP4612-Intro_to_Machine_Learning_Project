4fe21fca519de44f1e7c69277f15c854c8aba91d
Hector R <github.hector1@gmail.com>
2021-10-24 18:14:38 -0400

    LASSO DONE

--- ML Models/lasso.py
+++ ML Models/lasso.py
@@ -54,14 +54,16 @@ def init():
     ])
     search = GridSearchCV(pipeline,
                           {'model__alpha': np.arange(0.1, 10, 0.1)},
-                          cv=5, scoring="neg_mean_squared_error", verbose=3
-                          )
-    print(search.best_params_)
-    search.fit(d.get_training_split())
+                          cv=5, scoring="neg_mean_squared_error", verbose=1
+    )
+    X, Y = d.get_training_split()
+    search.fit(X, Y)
     print(search.best_params_)
     coefficients = search.best_estimator_.named_steps['model'].coef_
     importance = np.abs(coefficients)
-    print(np.array(importance))
+    print(f'Importance: {len(importance)}, Values: {importance}')
+    test = X.columns.to_numpy()[importance > 0]
+    print(test)
     """
     if(os.path.exists('alpha.csv')):
         print('Found alpha file, loading alpha value from file')
--- ML Models/recursive_feature_elimination.py
+++ ML Models/recursive_feature_elimination.py
@@ -4,8 +4,7 @@ from sklearn.feature_selection import RFECV
 import data as d
 
 
-splits = d.getSplits()
-X, y = splits[0], splits[2]
+X, y = d.get_training_split()
 
 # Create the RFE object and compute a cross-validated score.
 log_regression = LR()
@@ -17,4 +16,6 @@ rfecv = RFECV(estimator=log_regression, step=1, cv=StratifiedKFold(2),
               min_features_to_select=min_features_to_select)
 rfecv.fit(X, y)
 
-print("Optimal number of features : %d" % rfecv.n_features_)
+#print("Optimal number of features : %d" % rfecv.)
+print(f'Output Features: {rfecv.get_feature_names_out(X.columns)}')
+print(f'Trying feature_names_in_: {rfecv.feature_names_in_}')
